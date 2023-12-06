95544ae3fecbf6e32f643bc5ee31f9a020502d95
moxxing <vladdeman4ik@gmail.com>
2023-12-07 00:27:04 +0200

    2

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,34 @@
-const modalBtnElems = document.querySelectorAll(".btn-open");
-const modalElem = document.querySelector(".modalBackground");
+const modalController = ({ modal, btnOpen, btnClose }) => {
+  const modalBtnElems = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelector(modal);
 
-modalElem.style.cssText =
-  "display: flex; visibility: hidden; opacity: 0; transition: opacity 1000ms ease;";
+  modalElem.style.cssText =
+    "display: flex; visibility: hidden; opacity: 0; transition: opacity 1000ms ease;";
 
-const closeModal = (event) => {
-  const target = event.target;
+  const closeModal = (event) => {
+    const target = event.target;
 
-  if (target === modalElem || target.closest(".modalBtn")) {
-    modalElem.style.opacity = 0;
-    setTimeout(() => {
-      modalElem.style.visibility = "hidden";
-    }, 300);
-  }
+    if (target === modalElem || target.closest(btnClose)) {
+      modalElem.style.opacity = 0;
+      setTimeout(() => {
+        modalElem.style.visibility = "hidden";
+      }, 300);
+    }
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = "visible";
+    modalElem.style.opacity = 1;
+  };
+
+  modalBtnElems.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+  modalElem.addEventListener("click", closeModal);
 };
 
-const openModal = () => {
-  modalElem.style.visibility = "visible";
-  modalElem.style.opacity = 1;
-};
-
-modalBtnElems.forEach((btn) => {
-  btn.addEventListener("click", openModal);
+modalController({
+  modal: ".modalBackground",
+  btnOpen: ".btn-open",
+  btnClose: ".modalBtn",
 });
-modalElem.addEventListener("click", closeModal);
