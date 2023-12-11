d77d30ac1bdb04eddc0e276e9ba25af461f8205a
moxxing <vladdeman4ik@gmail.com>
2023-12-11 23:04:57 +0200

    2

--- js/secondModal.js
+++ js/secondModal.js
@@ -1,1 +1,19 @@
 const w = document.querySelector(".btn-q");
+const e = document.querySelector(".q");
+
+e.style.cssText =
+  "display: flex; visibility: hidden; opacity: 0; transition: opacity 3s ease";
+
+const closeQ = (event) => {
+  const targetQ = event.target;
+
+  if (targetQ === e) {
+    e.style.opacity = 0;
+    e.style.visibility = "hidden";
+  }
+};
+
+const openQ = () => {
+  e.style.visibility = "visible";
+  e.style.opacity = 1;
+};
